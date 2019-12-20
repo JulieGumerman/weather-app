982edee68292321f9917083c7c2e61b6d4fc33d0
bodhibrdr <julie.gumerman@gmail.com>
2019-12-20 11:03:13 -0700

    experimenting with place_name

--- app.js
+++ app.js
@@ -5,24 +5,26 @@ const address = process.argv[2]
 
 if (!address) {
     console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log("Error yo!!!", error)
+        }
+        console.log('data', data);
+        
+        weatherReport(data.latitude, data.longitude, data.place_name, (error, reportData) => {
+            if (error) {
+                return console.log(error)
+            } else {
+                console.log("DATA PLACE NAME", reportData.place_name)
+            }
+            
+            // console.log("data", reportData);
+            // console.log("location", reportData.place_name)
+        })
+    })    
 }
 
-geocode(address, (error, data) => {
-    if (error) {
-        return console.log("Error yo!!!", error)
-    }
-    console.log('data', data);
-    
-    weatherReport(data.latitude, data.longitude, data.place_name, (error, reportData) => {
-        if (error) {
-            return console.log(error)
-        }
-        console.log("DATA PLACE NAME", data.place_name)
-        // console.log("data", reportData);
-        // console.log("location", reportData.place_name)
-    })
 
-
-})
 
 
